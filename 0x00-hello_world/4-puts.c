99e63b42b877ee9c7088ae88d6ea1ea9ec8d97e5
danny21345 <zandilemkuzo@gmail.com>
2022-09-09 12:31:39 -0700

    First C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main: Print text followed by new line
+ *
+ * Description: That is achieved through puts funtion
+ * 
+ * Return: The program returns 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
