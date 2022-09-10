889fefc747c9324b42570e05ff3bc5e2f4da64d2
danny21345 <zandilemkuzo@gmail.com>
2022-09-09 20:24:14 -0700

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Uses the printf function to print a text
+*
+* This program use main to print given followed by new line
+*
+* Return: This program returns 0
+*
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
