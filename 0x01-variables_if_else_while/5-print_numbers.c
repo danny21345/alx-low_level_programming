2cff7c138bac607471b01d19f8a24d70d45a16b7
danny21345 <zandilemkuzo@gmail.com>
2022-09-12 02:09:19 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - prints numbers between 0 and 10
+*
+* I will use printf to achieve main
+*
+* Return: returns 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
