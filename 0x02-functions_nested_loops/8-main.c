b679c1482e18ad9f92fdb2ce9313cd2715dd2db1
danny21345 <zandilemkuzo@gmail.com>
2022-09-14 14:52:52 -0700

    Clock

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - print every minute of the day
+*
+* uses jack_bauer to achieve main
+*
+* Return: returns 0
+*/
+
+int main(void)
+{
+	jack_bauer();
+	return (0);
+
+}
