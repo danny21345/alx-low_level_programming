77d8ff2f2a1fc374170ad3889d99284ad151da28
danny21345 <zandilemkuzo@gmail.com>
2022-09-14 15:48:39 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* add - adds 2 nums and return sum
+* @a: first argument
+* @b: second argument
+*
+* simple arithmetic is applied
+*
+* Return: returns the sum
+*/
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - we print the result of 2 #s
+*
+* use add() to achieve main
+*
+* Return: returns 0
+*/
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+
+	return (0);
+}
