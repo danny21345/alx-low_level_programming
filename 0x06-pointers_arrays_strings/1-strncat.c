8f90a0aaf92ddbf61673e9b845d479962b584be2
danny21345 <zandilemkuzo@gmail.com>
2022-09-22 07:11:48 -0700

    Another Concatenation

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _strncat - Concatenates 2 strings
+* @dest: string to be concatenated to
+* @src: string to be concatenated by
+* @n: number of bytes
+*
+* Return: Destination pointer
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int d;
+	int f;
+
+	d = 0;
+	f = 0;
+
+	while (dest[0] != '\0')
+	{
+		d++;
+	}
+	while ((f < n) && src[f])
+	{
+		dest[d] = src[f];
+		d++;
+		f++;
+	}
+	dest[d + n + 1] = '\0';
+	return (dest);
+}
