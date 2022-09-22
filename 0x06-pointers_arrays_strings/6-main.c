fd84184ccd7aa585c6f37f72c26f19d7d9d52017
danny21345 <zandilemkuzo@gmail.com>
2022-09-22 13:32:58 -0700

    Capital

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -9,11 +9,19 @@
 
 int main(void)
 {
-	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-	char *ptr;
+	char str[] = "Expect the best. Prepare for the worst.";
+	char str1[] = " Capitalize on what comes.\nhello world! hello-world";
+	char str2[] = " 0123456hello world\thello world.hello world\n";
+	char *ptr, *ptr1, *ptr2;
 
 	ptr = cap_string(str);
 	printf("%s", ptr);
 	printf("%s", str);
+	ptr1 = cap_string(str1);
+	printf("%s", ptr1);
+	printf("%s", str1);
+	ptr2 = cap_string(str2);
+	printf("%s", ptr2);
+	printf("%s", str2);
 	return (0);
 }
